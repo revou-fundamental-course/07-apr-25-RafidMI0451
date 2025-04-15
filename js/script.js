bc310fc3a76ef47a27c9280eaba9fadb0350fa9f
RafidMI0451 <rafidygasli@gmail.com>
2025-04-15 23:40:21 +0700

    update 3_2

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Image Slider
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+initializeSlider();
+
+function initializeSlider(){
+
+    slides[slideIndex].classList.add("displaySlide");
+
+}
+function showSlide(Index){
+
+}
+function prevSlide(){
+
+}
+function nextSlide(){
+
+}
